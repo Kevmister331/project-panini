df8f1de957b23004ad44af6e485d34dcf37c3755
Kevin Hu <kevinh64@student.ubc.ca>
2023-01-21 14:39:15 -0800

    header

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>
+          <header>
+            <nav>
+            <img src="icons8-make-user-48.png" alt="logo" width="30%" height="30%"></img>
+            </nav>
+        </header>
+
+        <div className="main">
+
+      <section class="s1">
       <link href="https://fonts.googleapis.com/css2?family=Cute+Font&family=Fredoka+One&family=Kavoon&display=swap" rel="stylesheet"></link>
         <h1>Project Panini</h1>
+        <p>Type some prompts to get started!</p>
         <form className="search-box">
           <input type="search" placeholder="Type interests here" />
           <div></div>
         </form>
-      </header>
+      </section>
     </div>
+  </div>
   );
 }
 
