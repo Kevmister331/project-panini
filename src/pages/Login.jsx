c8a35c5c6b5e088738651ea0a77662b2d3313aa9
Kevin Hu <kevinh64@student.ubc.ca>
2023-01-22 02:50:48 -0800

    editing login styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,16 +12,17 @@ const Login = () => {
     return (
       <div className="auth-form-container">
       <form className="login-form" onSubmit={handleSubmit}>
-        <h2>Welcome Back!</h2>
-        <h3>Enter your details to sign</h3>
-        <h4>in to your account</h4>
+      <img id="panini2" src={require("../images/panini.png")} alt="logo"></img>
+      <h2 id="black">Welcome Back!</h2>
+        <p id="black">Enter your details to sign</p>
+        <p id="black">in to your account</p>
         <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="email" id="email" name="email"/>
         <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="password" id="password" name="password"/>
         <button2>Having trouble signing in?</button2>
-        <button3 type="submit">Log In</button3>
+        <button class="submit" type="submit">Log In</button>
         <h5>OR</h5>
-        <button3 type="submit">Sign up with email</button3>
-        <button4 type="submit">Sign up with Google</button4>
+        <button class="submit" type="submit">Sign up with email</button>
+        <button class="submit" type="submit">Sign up with Google</button>
       </form>
       </div>
     )
