9c93c9bd0d2a8854ddaa3d35406212e9761e2e3c
dylanlauzy <dylzylau@gmail.com>
2023-01-22 02:48:48 -0800

    Edited styles for selection page and generatoin page

--- src/pages/Generate.jsx
+++ src/pages/Generate.jsx
@@ -13,9 +13,9 @@ const Generate = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
-    const apiResult = await generate(keywordInput)
-    console.log(apiResult.choices[0].text);
-    setResult(apiResult.choices[0].text)
+    // const apiResult = await generate(keywordInput)
+    // console.log(apiResult.choices[0].text);
+    // setResult(apiResult.choices[0].text)
     setIsLoading(false);
     setSelectPage(true);
   }
@@ -34,13 +34,14 @@ const Generate = () => {
       <div className="main">
           <section class="s1">
             <img id="panini" src={require("../images/panini.png")} alt="logo"></img>
-            <h1>Project Panini</h1>
+            <h1 id="main-title">PROJECT PANINI</h1>
             <p>Type some prompts to get started!</p>
             <form className="search-box" onSubmit={onSubmit}>
               <input type="search"
                 placeholder="Type interests here"
                 value={keywordInput}
                 onChange={(e) => setKeywordInput(e.target.value)}/>
+              <button type="submit" className="search-btn">Search</button>
             </form>
             <div className="results">{result}</div>
           </section>
@@ -49,19 +50,19 @@ const Generate = () => {
   }
   
   if(selectPage) {
-    // let results = `"
+    let results = `"
 
-    // 1. Slam Dunk AI: Create an AI-driven program that can accurately predict the success rate of an NBA player's slam dunk. The program should take into account the player's skill level, the difficulty of the dunk, and the opposing team's defense.
+    1. Slam Dunk AI: Create an AI-driven program that can accurately predict the success rate of an NBA player's slam dunk. The program should take into account the player's skill level, the difficulty of the dunk, and the opposing team's defense.
     
-    // 2. Court Vision: Develop an AI-enabled system that can observe and analyze the movements of NBA players on the court in real-time and provide valuable insights on player performance.
+    2. Court Vision: Develop an AI-enabled system that can observe and analyze the movements of NBA players on the court in real-time and provide valuable insights on player performance.
     
-    // 3. AI-Driven Coaching: Create an AI-powered program that can suggest strategies and plays to NBA coaches based on the current game situation and the team's strengths and weaknesses.
+    3. AI-Driven Coaching: Create an AI-powered program that can suggest strategies and plays to NBA coaches based on the current game situation and the team's strengths and weaknesses.
     
-    // 4. Shot Analytics: Develop a program that can monitor and analyze the accuracy of shots taken by NBA players and provide feedback to coaches and players on their shooting performance.
+    4. Shot Analytics: Develop a program that can monitor and analyze the accuracy of shots taken by NBA players and provide feedback to coaches and players on their shooting performance.
     
-    // 5. Player Performance Modeling: Create an AI-driven system that can predict the performance of NBA players based on their past performance and current situation."`
+    5. Player Performance Modeling: Create an AI-driven system that can predict the performance of NBA players based on their past performance and current situation."`
 
-    let results = result.split('\n').filter((str) => str.length > 10).map(str => str.trim().slice(3)).map((str) => {
+    results = results.split('\n').filter((str) => str.length > 10).map(str => str.trim().slice(3)).map((str) => {
       let [name, description] = str.split(":").map(str => str.trim())
 
       return {name, description}
