ab035dec2878ffdba640d9936ab19c627e637e28
dylanlauzy <dylzylau@gmail.com>
2023-01-22 11:29:48 -0800

    Tweaking dashboard styles

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -11,10 +11,16 @@ const Dashboard = () => {
           <Dashboard2/>
         </div>
         <div className="middle">
+          <div classname="middle-container">
           <h1 id="unique">Court Vision</h1>
+          <div class="keyword-badges">
+            <div class="button">Machine Learning</div>
+            <div class="button">NBA</div>
+          </div>
           <div className="project-description">
             <p>Develop an AI-enabled system that can observe and analyze the movements of NBA players on the court in real-time and provide valuable insights on player performance.</p>
         </div>
+          </div>
         </div>
         <div className="sideright">
           <h1 id="poopy">Tasks List</h1>
