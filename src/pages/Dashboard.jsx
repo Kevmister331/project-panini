118c18bfb4ec5d2f37567d8547ad47f011cf7b8a
Kevin Hu <kevinh64@student.ubc.ca>
2023-01-21 20:21:51 -0800

    dashboard development

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,34 @@
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    
+    <div className="dashboard">
+      <link href="https://fonts.googleapis.com/css2?family=Cute+Font&family=Fredoka+One&family=Kavoon&display=swap" rel="stylesheet"></link>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+      <link rel="stylesheet" href="style.css"></link>
+
+
+
+        <div className="side">
+          <h1>My Projects</h1>
+          <form>
+            <input type="text" name="text" id="txt" required></input>
+            <button type="submit" class="btn-list">
+                <i class="fa-solid fa-plus"></i>
+            </button>
+          </form>
+          <div class="list-container">
+            <ul>
+              <li>Project 1<span>x</span></li>
+              <li>Project 2<span>x</span></li>
+              <li>Project 3<span>x</span></li>
+            </ul>
+
+          </div>
+        
+        </div>
+        <div className="middle"><p>hello</p></div>
+        <div className="side"><p>hello</p></div>
+    </div>
   )
 }
 
